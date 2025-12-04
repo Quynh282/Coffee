c012db67b23a68360023ecc794327a604cea9c8a
Quynh <quynhb2109666@student.ctu.edu.vn>
2025-12-04 19:58:09 +0700

    Configure vscode, eslint and prettier

---
+++ eslint.config.cjs
@@ -0,0 +1,19 @@
+const js = require('@eslint/js');
+const globals = require('globals');
+const eslintConfigPrettier = require('eslint-config-prettier');
+
+module.exports = [
+    {
+        languageOptions: {
+            ecmaVersion: 'latest',
+            sourceType: 'commonjs',
+            globals: {
+                ...globals.node,
+            },
+        },
+        files: ['**/*.{js,mjs,cjs}'],
+        ignores: ['{dist,public}/**/*']
+    },
+    js.configs.recommended,
+    eslintConfigPrettier
+];
