b7e76a9a85340fd64fd8c163f4c891ae42b9e303
Quynh <quynhb2109666@student.ctu.edu.vn>
2025-12-05 15:40:12 +0700

    Serve Swagger UI from OpenAPI/Swagger spec

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
 
 const JSend = require('./jsend');
 const productsRouter = require('./routes/products.router');
@@ -7,7 +8,7 @@ const {
     resourceNotFound,
     handleError,
 } = require('./controllers/errors.controller');
-
+const swaggerDocument = require('../docs/openapiSpec.json');
 const app = express();
 
 app.use(cors());
@@ -18,6 +19,7 @@ app.get('/', (req, res) => {
     return res.json(JSend.success({ message: 'Coffee API OK' }));
 });
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/public', express.static('public'));
 productsRouter.setup(app);
 
